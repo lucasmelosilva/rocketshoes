20b38cc2b51f1ed3140b7d475d08aa59e29e8f95
lucas melo <lucas.melo044@gmail.com>
2022-10-30 13:02:16 -0300

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <div id="container">
+      <nav>
+        <img src="./assets/logo.svg" alt="RocketShoes" />
+        <ul>
+          <li>Homem</li>
+          <li>Mulher</li>
+          <li>Criança</li>
+          <li>Customizar</li>
+        </ul>
+
+        <div>
+          <input type="text" name="" id="" placeholder="Pesquisar" />
+        </div>
+
+        <img src="./assets/icon-bag.svg" alt="Bag" />
+      </nav>
+    </div>
   )
 }
 
