8d9f30118fd7adbc1d58a998c482181929a2ae29
lucas melo <lucas.melo044@gmail.com>
2022-10-30 16:12:23 -0300

    add aside e estilizacao

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,22 @@ function App() {
 
         <img src="./assets/icon-bag.svg" alt="Bag" />
       </nav>
+
+      <div className='content'>
+        <aside>
+          <div>
+            <strong>Tênis Esportivo Para Corrida
+              Top</strong>
+            <span className='text-description'>
+              Esse tênis tem o intuito de te deixar ainda mais fitness, focado, e musculoso.
+            </span>
+            <span className='text-price'>
+              R$ 1.000,00
+            </span>
+            <button className='buy-button'>comprar</button>
+          </div>
+        </aside>
+      </div>
     </div>
   )
 }
